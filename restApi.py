55b3f1d5289fa595b7bb2911325d83f785dd583c
Lana Allingham <lanaallingham@googlemail.com>
2019-03-18 17:39:17 +0000

    [GET-ENDPOINTS] Check room availability

--- restApi.py
+++ restApi.py
@@ -15,15 +15,14 @@ def get_bookings_by_user(user):
    conn = sqlite3.connect("BOOKING.db")
    conn.row_factory = sqlite3.Row
    results = []
-   # Need to return room name, maybe change roomId? Also add meeting name?
    c = conn.cursor()
    c.execute("SELECT * from BOOKINGS WHERE UserId=%s" % user)
    for item in c.fetchall():
       c.execute("SELECT Location from ROOMS WHERE id=%s" % int(item[2]))
       location = str(c.fetchone()[0])
       c.execute("SELECT Name from ROOMS WHERE id=%s" % int(item[2]))
-      name = str(c.fetchone()[0])
-      row = json.loads('{ "BookingId":%s, "UserId":%s, "RoomId":%s, "StartTime":"%s", "EndTime":"%s", "Location": "%s", "RoomName": "%s"}' % (int(item[0]), int(item[1]), int(item[2]), str(item[3]), str(item[4]), location, name))
+      room_name = str(c.fetchone()[0])
+      row = json.loads('{ "BookingId":%s, "UserId":%s, "RoomId":%s, "MeetingName":"%s", "StartTime":"%s", "EndTime":"%s", "Location": "%s", "RoomName": "%s"}' % (int(item[0]), int(item[1]), int(item[2]), str(item[3]), str(item[4]), str(item[5]), location, room_name))
       results.append(row)
    
    return json.dumps(results)
@@ -37,34 +36,58 @@ def get_rooms_watched_by_user(user):
    c = conn.cursor()
    c.execute("SELECT * from WATCHED WHERE UserId=%s" % user)
    for item in c.fetchall():
-      row = json.loads('{ "WatchedId":%s, "UserId":%s, "Capacity":%s, "StartTime":"%s", "EndTime":"%s", "Location":"LOCATION PLACEHOLDER", "RoomName":"ROOM NAME PLACEHOLDER"}' % (int(item[0]), int(item[1]), int(item[2]), str(item[3]), str(item[4])))
+      c.execute("SELECT Location from ROOMS WHERE id=%s" % int(item[2]))
+      location = str(c.fetchone()[0])
+      c.execute("SELECT Name from ROOMS WHERE id=%s" % int(item[2]))
+      name = str(c.fetchone()[0])
+      c.execute("SELECT COUNT(*) from BOOKINGS, WATCHED WHERE BOOKINGS.RoomId=%s AND ((BOOKINGS.StartTime BETWEEN '%s' AND '%s') OR (BOOKINGS.EndTime BETWEEN '%s' AND '%s') OR ('%s' BETWEEN BOOKINGS.StartTime AND BOOKINGS.EndTime))" % (int(item[0]), str(item[4]), str(item[5]), str(item[4]), str(item[5]), str(item[4])))
+      if int(c.fetchone()[0]) == 0:
+         availability = 'Available'
+      else:
+         availability = 'Unavailable'
+      row = json.loads('{ "WatchedId":%s, "UserId":%s, "RoomId":%s, "Capacity":%s, "StartTime":"%s", "EndTime":"%s", "Location":"%s", "RoomName":"%s", "Availability":"%s"}' % (int(item[0]), int(item[1]), int(item[2]), int(item[3]), str(item[4]), str(item[5]), location, name, availability))
       results.append(row)
    
-   rows = c.fetchall()
    return json.dumps(results)
 
-# Needs '' when using the params, the defaults can't have ''. Maybe use like for the name so it's a search but not 100% match
-# This is messy, not sure how it would be in the front end if I did this so maybe need to do some ifs, or just make the default on the front end be the table names
-@app.route('/meeting-rooms', defaults={'loc': 'Location', 'floor': 'Floor', 'cap': 0})
-@app.route('/meeting-rooms/location/<loc>', defaults={'floor': 'Floor', 'cap': 0})
-@app.route('/meeting-rooms/location/<loc>/floor/<floor>', defaults={'cap': 0})
-@app.route('/meeting-rooms/location/<loc>/capacity/<int:cap>', defaults={'floor': 'Floor'})
-@app.route('/meeting-rooms/location/<loc>/floor/<floor>/capacity/<int:cap>')
-def find_meeting_room(loc, floor, cap):
+@app.route('/meeting-rooms',methods = ['GET', 'OPTIONS'])
+def find_meeting_room():
    conn = sqlite3.connect("BOOKING.db")
    conn.row_factory = sqlite3.Row
    results = []
-   
+     
    c = conn.cursor()
-   c.execute("SELECT * from ROOMS WHERE Location=%s AND Floor=%s AND Capacity>=%s" % (loc, floor, cap))
-   for item in c.fetchall():
-      row = json.loads('{ "id":%s, "Name":"%s", "Location":"%s", "Floor":"%s", "Capacity":%s}' % (int(item[0]), str(item[1]), str(item[2]), str(item[3]), int(item[4])))
-      results.append(row)
+   if request.args:
+      content = request.args
+      name = ("'%s'" % content.get('name').replace('%20', ' ')) if content.get('name') else 'Name'
+      loc = ("'%s'" % content.get('location').replace('%20', ' ')) if content.get('location') else 'Building'
+      floor = (content.get('floor')) if content.get('floor') else 'Floor'
+      cap = ("'%s'" % content.get('capacity')) if content.get('capacity') else 0
+      start = (content.get('start')) if content.get('start') else '9999-01-01 00:00:00'
+      end = (content.get('end')) if content.get('end') else '9999-01-01 00:00:00'
+      show_unavailable = (content.get('show_unavailable')) if content.get('show_unavailable') else 'false'
+      c.execute("SELECT * from ROOMS WHERE Name=%s AND Building=%s AND Floor=%s AND Capacity>=%s" % (name, loc, floor, cap))
+   else:
+      c.execute("SELECT * from ROOMS")
+      start = '9999-01-01 00:00:00'
+      end = '9999-01-01 00:00:00'
+      show_unavailable = 'false'
+   try:
+      for item in c.fetchall():
+         c.execute("SELECT COUNT(*) from BOOKINGS, WATCHED WHERE BOOKINGS.RoomId=%s AND ((BOOKINGS.StartTime BETWEEN '%s' AND '%s') OR (BOOKINGS.EndTime BETWEEN '%s' AND '%s') OR ('%s' BETWEEN BOOKINGS.StartTime AND BOOKINGS.EndTime))" % (int(item[0]), start, end, start, end, start))
+         if int(c.fetchone()[0]) == 0:
+            availability = 'Available'
+         else:
+            availability = 'Unavailable'
+         if not (show_unavailable == 'false' and availability == 'Unavailable'):
+            row = json.loads('{ "id":%s, "Name":"%s", "Location":"%s", "Floor":"%s", "Capacity":%s, "Building":"%s", "Availability":"%s"}' % (int(item[0]), str(item[1]), str(item[2]), str(item[3]), int(item[4]), str(item[5]), availability))
+            results.append(row)
+   except Exception as e:
+      print(e)
+      return '', 500
    
-   rows = c.fetchall()
    return json.dumps(results) 
 
-# get rid of this - or keep it to make system look more complete as book room button will work
 @app.route('/book-room',methods = ['POST', 'GET', 'OPTIONS'])
 def book_room():
    if request.method == 'POST':
@@ -73,31 +96,13 @@ def book_room():
          conn = sqlite3.connect('BOOKING.db')
          c = conn.cursor()
          c.execute("SELECT COUNT(*) from BOOKINGS")
-         watched_id = int(c.fetchone()[0]) + 1
-         c.execute("INSERT INTO BOOKINGS (BookingId,UserId,RoomId,StartTime,EndTime) VALUES (%i,%s,%s,'%s','%s')" % (watched_id, content['UserId'], content['RoomId'], content['StartTime'], content['EndTime']))
+         booking_id = int(c.fetchone()[0]) + 1
+         c.execute("INSERT INTO BOOKINGS (BookingId,UserId,RoomId,Name,StartTime,EndTime) VALUES (%i,'%s','%s','%s','%s','%s')" % (booking_id, content['UserId'], content['RoomId'], content['Name'], content['StartTime'], content['EndTime']))
          conn.commit()
          conn.close()
          return jsonify(content), 200
-      except:
-         return 'Error', 500
-   else:
-      return 'Not POST'
-
-# get rid of this
-@app.route('/add-room',methods = ['POST', 'GET', 'OPTIONS'])
-def watch_room():
-   if request.method == 'POST':
-      try:
-         content = request.json
-         conn = sqlite3.connect('BOOKING.db')
-         c = conn.cursor()
-         c.execute("SELECT COUNT(*) from ROOMS")
-         watched_id = int(c.fetchone()[0]) + 1
-         c.execute("INSERT INTO ROOMS (id,Name,Location,Floor,Capacity) VALUES (%i,'%s','%s','%s','%s')" % (watched_id, content['Name'], content['Location'], content['Floor'], content['Capacity']))
-         conn.commit()
-         conn.close()
-         return jsonify(content), 200
-      except:
+      except Exception as e:
+         print(e)
          return 'Error', 500
    else:
       return 'Not POST'
@@ -111,29 +116,15 @@ def watch_room():
          c = conn.cursor()
          c.execute("SELECT COUNT(*) from WATCHED")
          watched_id = int(c.fetchone()[0]) + 1
-         c.execute("INSERT INTO WATCHED (WatchedId,UserId,Capacity,StartTime,EndTime) VALUES (%i,%s,%s,'%s','%s')" % (watched_id, content['UserId'], content['Capacity'], content['StartTime'], content['EndTime']))
+         c.execute("INSERT INTO WATCHED (WatchedId,UserId,RoomId,Capacity,StartTime,EndTime) VALUES (%i,%s,%s,%s,'%s','%s')" % (watched_id, content['UserId'], content['RoomId'], content['Capacity'], content['StartTime'], content['EndTime']))
          conn.commit()
          conn.close()
          return jsonify(content), 200
-      except:
+      except Exception as e:
+         print(e)
          return 'Error', 500
    else:
       return 'Not POST'
-
-@app.route('/watched-rooms')
-def get_watched_rooms():
-   conn = sqlite3.connect("BOOKING.db")
-   conn.row_factory = sqlite3.Row
-   results = []
-   
-   c = conn.cursor()
-   c.execute("SELECT * from WATCHED")
-   for item in c.fetchall():
-      row = json.loads('{ "WatchedId":%s, "UserId":%s, "Capacity":%s, "StartTime":"%s", "EndTime":"%s"}' % (int(item[0]), int(item[1]), int(item[2]), str(item[3]), str(item[4])))
-      results.append(row)
-   
-   rows = c.fetchall()
-   return json.dumps(results)    
 
 @app.route('/delete-watch/<int:WatchId>', methods = ['DELETE', 'OPTIONS'])
 def delete_watched_rooms(WatchId):
